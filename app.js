e21e4197197ca84ea61167a4b65588dbad340077
Trevor Orsztynowicz <trevoro@joyent.com>
2012-03-08 23:45:56 -0800

    total rewrite

--- app.js
+++ app.js
@@ -1,59 +1,9 @@
 #!/usr/bin/env node
 
-var util = require('util');
-var dgram = require('dgram');
-var log = require('./lib/log');
-var named = require('./lib/named');
-var message = require('./lib/message');
+var Logger = require('bunyan')
+var named = require('./lib/index');
 
+var log = new Logger({name: 'namedjs', level: 'trace'});
 
-// config
-var config = {
-  host: 'localhost',
-  port: 9999
-}
-
-var onReceive = function(raw, client) {
-  var req;
-
-  try {
-    //req = message.parseDNS(raw, client);
-    req = named.parse(raw, client);
-  }
-  catch (error) {
-    // XXX respond with error if possible
-    // have some counter for errors
-    log.debug("error: issue parsing request");
-
-  }
-
-  log.debug("got DNS query");
- 
-  console.log(req);
-
-
-  named.buildResponse(req, function(err, raw) {
-    server.send(raw, 0, raw.length, client.port, client.address);
-  });
-
-}
-
-var server = dgram.createSocket('udp4');
-
-server.on('message', function(msg, rinfo) {
-  var raw = {
-    buf: msg,
-    len: rinfo.size
-  };
-  var client = {
-    family: 'udp4',
-    address: rinfo.address,
-    port: rinfo.port
-  };
-
-  onReceive(raw, client);
-
-});
-
-server.bind(config.port, config.host);
-log.info("dns server started on " + config.host + ":" + config.port);
+var agent = named.createAgent({log: log});
+agent.bind('udp4', 9999);
