beaa1f52135baf5f60bd4232bb3894e784db03d6
Trevor Orsztynowicz <trevoro@joyent.com>
2012-03-13 00:17:27 -0700

    protocol encode and decode test works 100%

--- test/samples.js
+++ test/samples.js
@@ -4,15 +4,17 @@
   using the 'dig' utility, as its the only tool that supports alternative ports
   when doing a query. 
 
-  When this module is loaded it will return a tiny helper function an a group
-  of samples that you can use to test serializers, protocol generators, create
-  a raw DNS client, etc.
+  When this module is loaded it will return an object containing an array of  
+  samples that you can use to test serializers, protocol generators, create a 
+  raw DNS client, etc.
 
-  Each sample is an Object with an 'id', 'description', and 'data'. The ID is used
-  so that adding and removing samples out of order will not affect external 
-  references to them in tests. The data is put through an encoder that will turn
-  this string into a raw buffer. That way, samples may be loaded from file that
-  can be read by a (mortal) human being.
+  Each sample is an object with an 'id', 'description', 'raw',  and 'data'. 
+  The ID is used so that adding and removing samples out of order will not affect 
+  external  references to them in tests. 
+  
+  The data is put through an encoder that will turn this string into a raw 
+  buffer. That way, samples may be loaded from file that can be read by a (mortal) 
+  human being.
 
   When the sample is encoded it places a "raw" value in the object. If you have one
   there it will be over-written.
@@ -23,6 +25,10 @@ var samples = [
   { id: 0,
   	description: "DNS QUERY - IN A NS1.JOYENT.DEV",
 		data: "0f 34 01 00 00 01 00 00 00 00 00 00 03 6e 73 31 06 6a 6f 79 65 6e 74 03 64 65 76 00 00 01 00 01",
+  },
+  { id: 1,
+  	description: "DNS QUERY - IN AAAA NS1.JOYENT.DEV",
+		data: "b9 dd 01 00 00 01 00 00 00 00 00 00 03 6e 73 31 06 6a 6f 79 65 6e 74 03 64 65 76 00 00 1c 00 01",
   }
 ];
 
@@ -38,18 +44,21 @@ var encode = function (data) {
 
 	for (i in tokens) {
     var t = '0x' + tokens[i]; 	 
-    buffer.writeInt8(parseInt(t), pos++);
+    var v = parseInt(t);
+    buffer.writeInt8(v, pos++, true);
 	}
-
 	return buffer;
 }
 
-var results = [];
-
-for (i in samples) {
-	var sample = samples[i];
-	sample.raw = encode(sample.data);
-	results.push(sample);
+var encodeSamples = function(samples) {
+	var sample, results = [];
+	for (i in samples) {
+		sample = samples[i];
+		sample.raw = encode(sample.data);
+		results.push(sample);
+	}
+	return results;
 }
 
-module.exports = results;
+
+module.exports = encodeSamples(samples);
--- test/test.js
+++ test/test.js
@@ -1,3 +1,38 @@
-#!/usr/bin/env node
+var samples = require('./samples');
+var protocol = require('../lib/protocol');
 
-var b = new Buffer(0);
+
+
+// helpers
+
+equalBuffers = function(b1, b2) {
+	if (b1.length !== b2.length) {
+		return false;
+	}
+
+	var l = b1.length;
+	while (l--) {
+		var one = b1.readUInt8(l);
+		var two = b2.readUInt8(l);
+		if (one !== two) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+var sample = samples[0];
+
+console.log(" >>> testing protocol decoder <<<");
+decoded = protocol.decode(sample.raw, 'queryMessage');
+console.log(JSON.stringify(decoded, null, 2));
+console.log(" >>> testing protocol encoder <<< ");
+encoded = protocol.encode(decoded.val, 'queryMessage');
+
+if (equalBuffers(encoded, sample.raw)) {
+	console.log("Encoder cycle passed!");
+}
+else {
+	console.log("Encoder cycle failed!");
+}
