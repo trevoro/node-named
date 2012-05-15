2590e97dd19a10321b376db976aefde605b09787
Trevor Orsztynowicz <trevoro@joyent.com>
2012-05-14 20:51:04 -0700

    fixing CNAME bugs

--- test/test.sh
+++ test/test.sh
@@ -2,7 +2,7 @@
 
 SERVER='localhost'
 PORT=9999
-HOST='ns1.relay.sh'
+HOST='ns1.example.com'
 DIGOPTS="+time=1 +short +retry=0"
 
 _fatal() {
@@ -10,55 +10,55 @@ _fatal() {
 }
 
 # lookup an A record
-printf "[%s:%s] '%s' type: %s" $SERVER $PORT $HOST 'A'
-out=$(dig @$SERVER -p $PORT -t A $HOST $DIGOPTS)
+printf "[%s:%s] '%s' type: %s" $SERVER $PORT 'ns1.example.com' 'A'
+out=$(dig @$SERVER -p $PORT -t A ns1.example.com $DIGOPTS)
 if [[ $? -ne 0 ]] ; then
 	_fatal "record lookup failed"
 else
-	printf "%20s\n" "OK"
+	printf "%30s\n" "OK"
 fi
 
 # lookup a CNAME record
-printf "[%s:%s] '%s' type: %s" $SERVER $PORT $HOST 'CNAME'
-out=$(dig @$SERVER -p $PORT -t CNAME $HOST $DIGOPTS)
+printf "[%s:%s] '%s' type: %s" $SERVER $PORT 'www.example.com' 'CNAME'
+out=$(dig @$SERVER -p $PORT -t CNAME www.example.com $DIGOPTS)
 if [[ $? -ne 0 ]] ; then
 	_fatal "record lookup failed"
 else
-	printf "%16s\n" "OK"
+	printf "%30s\n" "OK"
 fi
 
 # lookup a SOA record
-printf "[%s:%s] '%s' type: %s" $SERVER $PORT $HOST 'SOA'
-out=$(dig @$SERVER -p $PORT -t SOA relay.sh $DIGOPTS)
+printf "[%s:%s] '%s' type: %s" $SERVER $PORT 'example.com' 'SOA'
+out=$(dig @$SERVER -p $PORT -t SOA example.com $DIGOPTS)
 if [[ $? -ne 0 ]] ; then
 	_fatal "record lookup failed"
 else
-	printf "%18s\n" "OK"
+	printf "%30s\n" "OK"
 fi
 
 # lookup MX records
-printf "[%s:%s] '%s' type: %s" $SERVER $PORT $HOST 'MX'
-out=$(dig @$SERVER -p $PORT -t MX relay.sh $DIGOPTS)
+printf "[%s:%s] '%s' type: %s" $SERVER $PORT 'example.com' 'MX'
+out=$(dig @$SERVER -p $PORT -t MX example.com $DIGOPTS)
 if [[ $? -ne 0 ]] ; then
 	_fatal "record lookup failed"
 else
-	printf "%19s\n" "OK"
+	printf "%30s\n" "OK"
 fi
 
 # lookup AAAA records
 printf "[%s:%s] '%s' type: %s" $SERVER $PORT $HOST 'AAAA'
-out=$(dig @$SERVER -p $PORT -t AAAA relay.sh $DIGOPTS)
+out=$(dig @$SERVER -p $PORT -t AAAA example.com $DIGOPTS)
 if [[ $? -ne 0 ]] ; then
 	_fatal "record lookup failed"
 else
-	printf "%17s\n" "OK"
+	printf "%30s\n" "OK"
 fi
 
 # lookup SRV records
-printf "[%s:%s] '%s' type: %s" $SERVER $PORT "_sip._tcp.relay.sh" 'AAAA'
-out=$(dig @$SERVER -p $PORT -t SRV _sip._tcp.relay.sh $DIGOPTS)
+printf "[%s:%s] '%s' type: %s" $SERVER $PORT "_sip._tcp.example.com" 'SRV'
+out=$(dig @$SERVER -p $PORT -t SRV _sip._tcp.example.com $DIGOPTS)
 if [[ $? -ne 0 ]] ; then
 	_fatal "record lookup failed"
 else
-	printf "%17s\n" "OK"
+	printf "%30s\n" "OK"
 fi
